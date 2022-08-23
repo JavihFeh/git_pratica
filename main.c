1880d38c369686be34d4129e452f4e576cffd5ef
JavihFeh <37805872+JavihFeh@users.noreply.github.com>
2022-08-23 11:20:09 -0300

    v0.1
    pronto para branch

--- main.c
+++ main.c
@@ -2,7 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
-void main(){
-  printf("ola");
+int main()
+{
+  srand(time(NULL))
+  int i, vet[1000];
+  for(i=0; i < 1000; i++)
+      vet[i] = rand();
 }
