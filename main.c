b522d5514dc14a0dcb9ebfe0be734b6252409426
Gui019 <111884129+Gui019@users.noreply.github.com>
2022-08-26 16:12:11 -0300

    final

--- main.c
+++ main.c
@@ -2,13 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <time.h>
 
-int main()
-{
-  int i, vet[1000];
-  
-  
-  
-  
-  return 0;
+int main() {
+
+    int i, vet[1000], maior, menor;
+
+    srand(time(NULL));
+
+    for(i = 0; i < 1000; i++)
+        vet[i] = rand();
+    
+    for(i = 0; i < 1000; i++)
+        printf("%d ", vet[i]);
+    
+    maior = vet[0];   
+    for(i = 0; i < 1000; i++)
+    {
+        if (vet[i] > maior)
+        {
+            maior=vet[i];
+        }
+    }
+    
+    menor = vet[0];
+    for(i = 0; i < 1000; i++)
+    {
+        if (vet[i] < menor)
+        {
+            menor=vet[i];
+        }
+    }
+    
+    printf("\n\n");
+    printf("Maior valor: %d ", maior);
+    
+    printf("\n\n");
+    printf("Menor valor: %d ", menor);
+    
+
+    return 0;
 }
